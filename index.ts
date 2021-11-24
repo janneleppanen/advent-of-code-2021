921c04318a3678f0481f1bed9a57aaf045878608
Janne Leppänen <janne@janneleppanen.com>
2021-11-24 18:14:49 +0200

    chore(setup): Add run command

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const day = parseInt(process.argv[2]) || "all";
+
+if (day === "all") {
+  for (let d = 1; d < 26; d++) {
+    try {
+      require(`./day-${d}`);
+    } catch (e) {}
+  }
+} else {
+  try {
+    require(`./day-${day}`);
+  } catch (e) {
+    console.log(`Solution not found for day ${day}`);
+  }
+}
