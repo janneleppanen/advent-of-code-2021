b7be96b3d906a694d2fdf7465f6c3fa2bfdbf403
Janne Leppänen <janne@janneleppanen.com>
2021-12-01 18:29:14 +0200

    fix(setup): Fix run script

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
-const day = parseInt(process.argv[2]) || "all";
+const run = () => {
+  const day = parseInt(process.argv[2]) || "all";
 
-if (day === "all") {
-  for (let d = 1; d < 26; d++) {
-    try {
-      require(`./day-${d}`);
-    } catch (e) {}
+  if (day === "all") {
+    for (let d = 1; d < 26; d++) {
+      try {
+        import(`./day-${d}`);
+      } catch (e) {}
+    }
+  } else {
+    import(`./day-${day}`);
   }
-} else {
-  require(`./day-${day}`);
-}
+};
+
+run();
--- utils/run.ts
+++ utils/run.ts
@@ -1,4 +1,4 @@
-import fs from "fs";
+import * as fs from "fs";
 
 type Callback = (input: string, resolve: Function) => void;
 
