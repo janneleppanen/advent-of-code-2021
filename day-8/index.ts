9ab2bbc9e65d74f42d03d262e54c6505cdb62b86
Janne Leppänen <janne@janneleppanen.com>
2021-12-08 19:03:05 +0200

    refactor(day-8): Clean solution a bit

--- day-8/index.ts
+++ day-8/index.ts
@@ -8,17 +8,15 @@ import {
 run(__dirname, (input: string, resolve: Function) => {
   const entries = parseEntries(input);
   const numbersToFind = [1, 4, 7, 8];
-  let countOfPart1Numbers = 0;
 
-  entries.forEach((entry) => {
+  let countOf1478 = entries.reduce((sum, entry) => {
     const converter = getPatternConverter(entry.patterns);
     const numbers = convertOutputToNumbers(entry.output, converter);
-    countOfPart1Numbers += numbers.filter((n) =>
-      numbersToFind.includes(n)
-    ).length;
-  });
 
-  resolve(countOfPart1Numbers);
+    return sum + numbers.filter((n) => numbersToFind.includes(n)).length;
+  }, 0);
+
+  resolve(countOf1478);
 
   const sum = entries.reduce((sum, entry) => {
     const converter = getPatternConverter(entry.patterns);
