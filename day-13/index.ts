f82a937c7ec28b480cc222f073bb40d8e810a1b5
Janne Leppänen <janne@janneleppanen.com>
2021-12-13 18:51:38 +0200

    feat(day-13): Add solution for the part 2

--- day-13/index.ts
+++ day-13/index.ts
@@ -5,6 +5,8 @@ run(__dirname, (input: string, resolve: Function) => {
   const { dots, foldInstructions } = parseInput(input);
   const afterFirstFold = fold(dots, foldInstructions[0]);
   const dotCount = countDots(afterFirstFold);
-
   resolve(dotCount);
+
+  const afterAllFolds = foldInstructions.reduce(fold, dots);
+  resolve(`\n${getVisualMap(afterAllFolds)}`);
 });
